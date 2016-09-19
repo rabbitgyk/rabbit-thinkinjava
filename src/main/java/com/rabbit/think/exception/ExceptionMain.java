6c4703019a82652960c04fe896e4746a1eed1488
guoyankui <yankuiguo@gmail.com>
2016-09-19 23:24:33 +0800

    添加自定义异常的使用描述

--- src/main/java/com/rabbit/think/exception/ExceptionMain.java
+++ src/main/java/com/rabbit/think/exception/ExceptionMain.java
@@ -1,5 +1,10 @@
 package com.rabbit.think.exception;
 
+/**
+ * 自定义异常的使用方法，修改getName方法的入参age的值能看到不同的异常构造形式
+ * @author rabbit
+ * 2016年9月19日
+ */
 public class ExceptionMain {
 
 	public static void main(String[] args) {
@@ -15,4 +20,6 @@ public class ExceptionMain {
 		}
 		System.out.println("查到名字：" + name);
 	}
+	
+	
 }
